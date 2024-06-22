fe6f0dffee7bb0e49850d10c7692cd5ea3089467
GaganVashisht <gagandeepvashisht96@gmail.com>
2024-06-22 18:40:59 +0530

    Added api gateway

--- UserService/src/main/java/com/project/user/service/services/impl/UserServicesImp.java
+++ UserService/src/main/java/com/project/user/service/services/impl/UserServicesImp.java
@@ -5,11 +5,8 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.project.user.service.dto.HotelDto;
 import com.project.user.service.dto.RatingDto;
