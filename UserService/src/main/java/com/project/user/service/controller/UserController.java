e67b87e29d9411cab42e040c56e39943005443c8
GaganVashisht <gagandeepvashisht96@gmail.com>
2024-07-16 23:34:57 +0530

    okta security added in rating and hotel service

--- UserService/src/main/java/com/project/user/service/controller/UserController.java
+++ UserService/src/main/java/com/project/user/service/controller/UserController.java
@@ -2,26 +2,8 @@ package com.project.user.service.controller;
 
 import java.util.List;
 
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.project.user.service.dto.UserDto;
 import com.project.user.service.services.UserService;
-
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("/users")
